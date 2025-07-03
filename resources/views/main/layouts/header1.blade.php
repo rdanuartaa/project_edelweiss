4c2a3e6dd9de4702c596098761d5926516c2bc95
Rizal Danuarta Akbar <akbardanu631@gmail.com>
2025-07-03 19:46:22 +0700

    new

---
+++ resources/views/main/layouts/header1.blade.php
@@ -0,0 +1,68 @@
+  <header class="header-area header-two transparent-header">
+        <!--====== Header Navigation ======-->
+        <div class="header-navigation navigation-white">
+            <div class="nav-overlay"></div>
+            <div class="container-fluid">
+                <div class="primary-menu">
+                    <!--====== Site Branding ======-->
+                    <div class="site-branding">
+                        <a href="index.html" class="brand-logo"><img src="assets/images/logo/logo-white.png"
+                                alt="Site Logo"></a>
+                    </div>
+                    <!--====== Nav Menu ======-->
+                    <div class="nav-menu">
+                        <!--====== Site Branding ======-->
+                        <div class="mobile-logo mb-30 d-block d-xl-none">
+                            <a href="index.html" class="brand-logo"><img src="assets/images/logo/logo-black.png"
+                                    alt="Site Logo"></a>
+                        </div>
+                        <!--=== Nav Search ===-->
+                        <div class="nav-search mb-30 d-block d-xl-none ">
+                            <form>
+                                <div class="form_group">
+                                    <input type="email" class="form_control" placeholder="Search Here" name="email"
+                                        required>
+                                    <button class="search-btn"><i class="fas fa-search"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                        <!--====== Main Menu ======-->
+                        <nav class="main-menu">
+                            <ul>
+                                <li class="menu-item has-children"><a href="#">Home</a></li>
+                                <li class="menu-item has-children"><a href="#">Tours</a></li>
+                                <li class="menu-item has-children"><a href="#">Destination</a></li>
+                                <li class="menu-item has-children"><a href="#">Blog</a></li>
+                                <li class="menu-item has-children"><a href="#">Pages</a></li>
+                            </ul>
+                        </nav>
+                        <!--====== Menu Button ======-->
+                        <div class="menu-button mt-40 d-xl-none">
+                            <a href="contact.html" class="main-btn secondary-btn">Book Now<i
+                                    class="fas fa-paper-plane"></i></a>
+                        </div>
+                    </div>
+                    <!--====== Nav Right Item ======-->
+                    <div class="nav-right-item">
+                        <div class="search-btn" data-bs-toggle="modal" data-bs-target="#search-modal"><i
+                                class="far fa-search"></i></div>
+                        <div class="lang-dropdown">
+                            <select class="wide">
+                                <option value="English">English</option>
+                                <option value="French">French</option>
+                            </select>
+                        </div>
+                        <div class="menu-button d-xl-block d-none">
+                            <a href="contact.html" class="main-btn primary-btn">Book Now<i
+                                    class="fas fa-paper-plane"></i></a>
+                        </div>
+                        <div class="navbar-toggler">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header><!--====== End Header ======-->
